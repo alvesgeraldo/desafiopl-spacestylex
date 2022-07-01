b5033cf5ac9c64a2830aabbeb603f6faa608b1e9
Geraldo Alves <92646470+alvesgeraldo@users.noreply.github.com>
2022-07-01 15:55:09 -0300

    Ajustes de JS, botão voltar ao topo, ajuste no rodapé

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,9 @@
     let fone = document.getElementById('fone');
     let mapa = document.getElementById('mapa');
 
+
+    console.log(btntop);
+
     function mudarEndereco(dados){
 
       if (dados == 'matriz') {
@@ -33,6 +36,8 @@
         cidade.innerHTML = 'NY - Estados Unidos';
         cep.innerHTML = '';
         fone.innerHTML = '+17185934204';
+        mapa.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24216.061671997646!2d-73.85964387070996!3d40.651760466109884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2678125152133%3A0x82eddafb0028c7dc!2s163-50%20Cross%20Bay%20Blvd%2C%20Howard%20Beach%2C%20NY%2011414%2C%20EUA!5e0!3m2!1spt-BR!2sbr!4v1656694215336!5m2!1spt-BR!2sbr';
+
 
       } else if(dados == 'filial3'){
 
@@ -41,6 +46,8 @@
         cidade.innerHTML = 'Lisboa, Portugal';
         cep.innerHTML = '1750-184';
         fone.innerHTML = '+351217501200';
+        mapa.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12443.252111668127!2d-9.160015058034865!3d38.76799221231055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1932f71fdbdbe9%3A0x8ff008ad33fadccd!2sAv.%20Maria%20Helena%20Vieira%20da%20Silva%2048%2C%201750-184%20Lisboa%2C%20Portugal!5e0!3m2!1spt-BR!2sbr!4v1656694292822!5m2!1spt-BR!2sbr';
+
 
       }
     }
